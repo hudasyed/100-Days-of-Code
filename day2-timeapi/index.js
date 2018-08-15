66a063b2a344e9b7af474c3681aa1956e960c301
Huda Syed <who.the.syed@gmail.com>
2018-08-14 22:07:42 -0700

    handling timezone formatting errors, changing default format to hh:mm:ss

--- day2-timeapi/index.js
+++ day2-timeapi/index.js
@@ -8,13 +8,20 @@ var router = express.Router();
 router.get('/', function(req, res){
     const timezone = req.query.timezone;
     let format = req.query.format;
+    
+    // error handling
     if(!timezone)
     {
         res.status(400).send('You have not defined a timezone');
     }
+    if(!moment.tz.zone(timezone))
+    {
+        res.status(400).send('You have not defined a correct timezone');
+    }
+
     if(!format)
     {
-        format = 'h:m:s'
+        format = 'hh:mm:ss'
     }
     const time = moment().tz(timezone).format(format);
     res.json(time);
