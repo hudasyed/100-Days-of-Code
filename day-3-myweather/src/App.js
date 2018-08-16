67f4aecb4ea6dd984df11bf1567c9e14fa2d781e
Huda Syed <who.the.syed@gmail.com>
2018-08-15 21:50:34 -0700

    Setting up background, favicon and geolocation api.

--- day-3-myweather/src/App.js
+++ day-3-myweather/src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Helmet } from 'react-helmet';
 import './App.css';
 
 class App extends Component {
+  location() {
+    return navigator.geolocation.getCurrentPosition(function success(position){
+      console.log(position);
+      return position;
+    }, function error() {
+      return null;
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Helmet>
+          <style>{'body {background-color: #3e939b;}'}</style>
+          <p>Your location is {this.location()}</p>
+        </Helmet>
       </div>
     );
   }
 }
+
 
 export default App;
